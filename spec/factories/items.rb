a5370788ae2a618bccb33b8da176873bf5ae981e
lefmarna <panume5ya@gmail.com>
2021-03-31 17:09:26 +0900

    itemモデルの単体テストコードを記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,4 +1,17 @@
 FactoryBot.define do
   factory :item do
+    name                  { Faker::Name.name }
+    description           { Faker::Lorem.sentence }
+    category_id           { Faker::Number.number(digits: 3) }
+    status_id             { Faker::Number.number(digits: 3) }
+    shipping_fee_id       { Faker::Number.number(digits: 3) }
+    prefecture_id         { Faker::Number.number(digits: 3) }
+    scheduled_delivery_id { Faker::Number.number(digits: 3) }
+    price                 { Faker::Number.number(digits: 7) }
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('app/assets/images/dl-android.png'), filename: 'dl-android.png')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,99 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品機能' do
+    context '商品を出品できるとき' do
+      it '全ての情報が正しく入力されていれば出品できる' do
+        expect(@item).to be_valid
+      end
+
+      it '販売価格が300円以上であれば出品できる' do
+        @item.price = 300
+        expect(@item).to be_valid
+      end
+
+      it '販売価格が9,999,999円以下であれば出品できる' do
+        @item.price = 9_999_999
+        expect(@item).to be_valid
+      end
+    end
+
+    context '商品を出品できないとき' do
+      it '出品画像が空だと出品できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+
+      it '商品名が空だと出品できない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+
+      it '商品の説明が空だと出品できない' do
+        @item.description = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
+      end
+
+      it 'カテゴリーが空だと出品できない' do
+        @item.category_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Category must be other than 0')
+      end
+
+      it '商品の状態が空だと出品できない' do
+        @item.status_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Status must be other than 0')
+      end
+
+      it '配送料の負担が空だと出品できない' do
+        @item.shipping_fee_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Shipping fee must be other than 0')
+      end
+
+      it '発送元の地域が空だと出品できない' do
+        @item.prefecture_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Prefecture must be other than 0')
+      end
+
+      it '発送までの日数が空だと出品できない' do
+        @item.scheduled_delivery_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Scheduled delivery must be other than 0')
+      end
+
+      it '販売価格が空だと出品できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+
+      it '販売価格が299円以下だと出品できない' do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
+      end
+
+      it '販売価格が10,000,000円以上だと出品できない' do
+        @item.price = 10_000_000
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
+      end
+
+      it '販売価格は半角数字でないと出品できない' do
+        @item.price = '２０００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is not a number')
+      end
+    end
+  end
 end
