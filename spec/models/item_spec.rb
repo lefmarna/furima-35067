3d5b4516e27b9136c350dc1e2ebf1201321ccd55
lefmarna <panume5ya@gmail.com>
2021-03-31 17:42:34 +0900

    テストコードを1つ追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -94,6 +94,12 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is not a number')
       end
+
+      it 'ユーザーが存在していないと出品できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
+      end
     end
   end
 end
