26334cef8bc77a5988a75ff905f2d62e6441c282
lefmarna <panume5ya@gmail.com>
2021-03-31 14:32:03 +0900

    商品投稿機能の実装

---
+++ app/models/item.rb
@@ -0,0 +1,25 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  has_one_attached :image
+  belongs_to :user
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+    with_options numericality: { other_than: 0 } do
+      validates :category_id
+      validates :status_id
+      validates :shipping_fee_id
+      validates :prefecture_id
+      validates :scheduled_delivery_id
+    end
+  end
+end
