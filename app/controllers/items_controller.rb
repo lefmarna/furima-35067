5db7adb6d8e0cd734d00801318f81d45be9a685d
lefmarna <panume5ya@gmail.com>
2021-04-01 16:08:17 +0900

    コードの整形

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -40,6 +40,7 @@ class ItemsController < ApplicationController
   end
 
   private
+
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :status_id, :shipping_fee_id, :prefecture_id,
                                  :scheduled_delivery_id, :price).merge(user_id: current_user.id)
