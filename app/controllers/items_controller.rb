d72c0f0f49c48adfe247508482cd0f5718faafad
lefmarna <panume5ya@gmail.com>
2021-04-01 14:16:06 +0900

    商品情報編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,19 @@ class ItemsController < ApplicationController
     end
   end
 
+  Z def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
+  end
+
   def show
     @item = Item.find(params[:id])
   end
