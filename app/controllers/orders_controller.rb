2fe0a5fa004d70c3635ba879181e3f752c2e1fef
lefmarna <panume5ya@gmail.com>
2021-04-02 15:37:20 +0900

    PAY.JPを用いたクレジットカードでの決済機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,25 +10,35 @@ class OrdersController < ApplicationController
   def create
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+      pay_item
       @order_address.save
       redirect_to root_path
     else
       render :index
     end
   end
-  
+
   private
+
   def set_item
     @item = Item.find(params[:item_id])
   end
 
   def check_user
-    if current_user.id == @item.user.id
-      redirect_to root_path
-    end
+    redirect_to root_path if current_user.id == @item.user.id
   end
 
   def order_params
-    params.require(:order_address).permit(:zip, :prefecture, :city, :address_line1, :address_line2, :phone_number).merge(user_id: current_user.id).merge(item_id: @item.id)
+    params.require(:order_address).permit(:zip, :prefecture, :city, :address_line1, :address_line2,
+                                          :phone_number).merge(user_id: current_user.id).merge(item_id: @item.id).merge(token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :zip, :prefecture, :city, :address_line1, :address_line2, :phone_number
+  attr_accessor :user_id, :item_id, :zip, :prefecture, :city, :address_line1, :address_line2, :phone_number, :token
 
   with_options presence: true do
     validates :zip, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
@@ -8,6 +8,7 @@ class OrderAddress
     validates :city
     validates :address_line1
     validates :phone_number
+    validates :token
   end
 
   def save
