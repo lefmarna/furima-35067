f06c6d9462f2eef644473ffcd54cebf1dd7d3287
lefmarna <panume5ya@gmail.com>
2021-04-12 12:46:44 +0900

    SNSでのログイン機能を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,4 +26,18 @@ class User < ApplicationRecord
 
   validates :password,
             format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d!-~]+\z/i, message: 'is invalid. Include both letters and numbers.' }
+
+  def self.from_omniauth(auth)
+    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+    user = User.where(email: auth.info.email).first_or_initialize(
+      nickname: auth.info.name,
+      email: auth.info.email
+    )
+
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    { user: user, sns: sns }
+  end
 end
